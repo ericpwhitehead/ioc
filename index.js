6cb3ad34dddd8d8ca2ff07022197cc52ff838b4d
Eric Whitehead <ericpwhitehead@gmail.com>
2018-07-24 09:36:01 -0400

    start port

--- index.js
+++ index.js
@@ -68,20 +68,20 @@ app.post('/', (req, res) => {
 	var len = Object.keys(postBody).length;
 	console.log(len)
 
-	if(postBody.hasOwnProperty('field_sex') && len > 4){
-		console.log('this is a new member')
-		rand = Math.floor(Math.random()*90000) + 10000;
-		console.log(rand)
-		dbConnection.query('INSERT into `users` (uid, name, pass, mail) VALUES (?,?,?,?)',[rand, postBody.name, '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', postBody.mail], function(err, result) {
-      			if (err) throw err
-			      result.forEach(function(row) {
-			      	console.log(row);
-			      	sockConn.dispose();
-			      });
+	// if(postBody.hasOwnProperty('field_sex') && len > 4){
+	// 	console.log('this is a new member')
+	// 	rand = Math.floor(Math.random()*90000) + 10000;
+	// 	console.log(rand)
+	// 	dbConnection.query('INSERT into `users` (uid, name, pass, mail) VALUES (?,?,?,?)',[rand, postBody.name, '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', postBody.mail], function(err, result) {
+ //      			if (err) throw err
+	// 		      result.forEach(function(row) {
+	// 		      	console.log(row);
+	// 		      	sockConn.dispose();
+	// 		      });
 
-		//(5060, 'testuser.three_5060', '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', 'testuser.three@instituteofcoaching.org', '', '', 'filtered_html', 1528218222, 1528985314, 1528985314, 1, 'America/New_York', '', 0, 'testuser.three@instituteofcoaching.org', 0x613a353a7b733a31363a22636b656469746f725f64656661756c74223b733a313a2274223b733a32303a22636b656469746f725f73686f775f746f67676c65223b733a313a2274223b733a31343a22636b656469746f725f7769647468223b733a343a2231303025223b733a31333a22636b656469746f725f6c616e67223b733a323a22656e223b733a31383a22636b656469746f725f6175746f5f6c616e67223b733a313a2274223b7d);
-	});
-	}
+	// 	//(5060, 'testuser.three_5060', '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', 'testuser.three@instituteofcoaching.org', '', '', 'filtered_html', 1528218222, 1528985314, 1528985314, 1, 'America/New_York', '', 0, 'testuser.three@instituteofcoaching.org', 0x613a353a7b733a31363a22636b656469746f725f64656661756c74223b733a313a2274223b733a32303a22636b656469746f725f73686f775f746f67676c65223b733a313a2274223b733a31343a22636b656469746f725f7769647468223b733a343a2231303025223b733a31333a22636b656469746f725f6c616e67223b733a323a22656e223b733a31383a22636b656469746f725f6175746f5f6c616e67223b733a313a2274223b7d);
+	// });
+	// }
 
 	
 
@@ -92,6 +92,7 @@ app.post('/', (req, res) => {
 		
 		dbConnection.query('SELECT * FROM `field_data_field_infusionsoft_id` WHERE field_infusionsoft_id_value = ?',[field_infusionsoft_id_value], function(err, result) {
       			if (err) throw err
+      				console.log('result from query', result);
 			      result.forEach(function(row) {
 			      	const userId = row.entity_id;
 				    var dateString = postBody['field_start_date:end'];
