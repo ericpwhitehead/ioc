ee16a9e2e1f2ebb5cf217b9cd3cd40b9fbdb248a
Eric Whitehead <ericpwhitehead@gmail.com>
2018-08-06 12:17:36 -0400

    testing again

--- index.js
+++ index.js
@@ -67,6 +67,27 @@ app.post('/test', (req, res) => {
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+function clearCache(id) {
+var myPromise = new Promise(function(resolve, reject){
+   // clear cache
+		  dbConnection.query('SELECT * FROM `users` WHERE uid = ?', [id], function(err, result2) {
+				if (err) reject(err)
+
+				console.log('result2', result2)
+		      result2.forEach(function(row) {
+		      	console.log('clear cache for...', row.name);
+		      	dbConnection.query('DELETE FROM `cache_entity_user` WHERE data LIKE ?',['%'+row.name+'%'], function(err, result3) {
+		      			console.log('cache cleared', result3)
+						if (err) throw err
+						console.log('result3', result3);
+						resolve(result3)
+	      			})
+			  })
+		  
+			  })
+	})
+}
+
 app.post('/', (req, res) => {
 
 	console.log('content type passed', req.headers['content-type']);
@@ -113,7 +134,14 @@ app.post('/', (req, res) => {
 			     // result.forEach(function(row) {
 			      	console.log('result0 ', result[0]);
 
-			      	console.log('result0 entity_id', result[0].entity_id)
+			      	var entity = result[0].entity_id;
+			      	clearCache(entity)
+			      	.then((resp) => {
+			      		console.log(resp);
+			      	})
+			      	.catch((err) => {
+			      		console.log(resp);
+			      	})
 			  //     	const userId = row.entity_id;
 				 //    var dateString = postBody['field_start_date:end'];
 					// var newDate = new Date(dateString);
