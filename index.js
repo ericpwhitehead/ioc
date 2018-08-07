5616ddf762b57b461209fd2c30fe558159bb662a
Eric Whitehead <ericpwhitehead@gmail.com>
2018-08-07 11:55:28 -0400

    test4

--- index.js
+++ index.js
@@ -127,16 +127,14 @@ app.post('/', (req, res) => {
 		console.log('this random number', rand);
 		console.log('name', postBody.name);
 		console.log('email', postBody.mail);
+		console.log('rand', rand)
+		// password1ioc
 		dbConnection.query('INSERT into "users" (uid, name, pass, mail) VALUES (?,?,?,?)',[rand, postBody.name, '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', postBody.mail], function(err, result) {
 			console.log('result', result);
 			console.log('err', err)
       			if (err) throw err
-
-			      result.forEach(function(row) {
-			      	console.log('this row', row);
-			      	sockConn.dispose();
-			      	res.json({message: 'new user added'})
-			      });
+      		res.json({message: 'new user added'})
+      	
 
 		//(5060, 'testuser.three_5060', '$S$Dyqk85Tk9TLeJ3SHRJ.6UL7yujsihBBRlzqKE6y3mKrHUP6/YNrP', 'testuser.three@instituteofcoaching.org', '', '', 'filtered_html', 1528218222, 1528985314, 1528985314, 1, 'America/New_York', '', 0, 'testuser.three@instituteofcoaching.org', 0x613a353a7b733a31363a22636b656469746f725f64656661756c74223b733a313a2274223b733a32303a22636b656469746f725f73686f775f746f67676c65223b733a313a2274223b733a31343a22636b656469746f725f7769647468223b733a343a2231303025223b733a31333a22636b656469746f725f6c616e67223b733a323a22656e223b733a31383a22636b656469746f725f6175746f5f6c616e67223b733a313a2274223b7d);
 	});
