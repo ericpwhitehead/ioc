8056aa12aaf9b28dea29b21229c5264a42cb9dc7
Eric Whitehead <ericpwhitehead@gmail.com>
2019-04-12 13:07:28 -0400

    inserting mail for UN

--- index.js
+++ index.js
@@ -247,8 +247,11 @@ app.post('/', (req, res) => {
 		console.log('rand', rand)
 		console.log('infusionsoft id', postBody.field_infusionsoft_id)
 		var rightNow = Date.now();
+		var firstName = postBody.field_infusionsoft_id;
+
 		// password1ioc
-		var values = [rand, postBody.name, '$S$D26haBLqyyr4d5lvfmWMyjDH6Can/no3t1tsREuiHnXUzGbJdNnn', postBody.mail, 'filtered_html', rightNow, 1, postBody.mail]
+		//var values = [rand, postBody.name, '$S$D26haBLqyyr4d5lvfmWMyjDH6Can/no3t1tsREuiHnXUzGbJdNnn', postBody.mail, 'filtered_html', rightNow, 1, postBody.mail]
+		var values = [rand, postBody.mail, '$S$D26haBLqyyr4d5lvfmWMyjDH6Can/no3t1tsREuiHnXUzGbJdNnn', postBody.mail, 'filtered_html', rightNow, 1, postBody.mail]
 		console.log('values', values);
 		console.log('val len', values.length)
 
