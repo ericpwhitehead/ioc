4d823d4c9f79c4cbed9447c1ad73c7bbee503598
Eric Whitehead <ericpwhitehead@gmail.com>
2019-05-16 16:16:43 -0400

    removing state mapping

--- index.js
+++ index.js
@@ -480,7 +480,12 @@ function updateAddressInfo(postBody, uid) {
 			dbConnection.query('UPDATE `field_data_field_member_address` SET `field_member_address_thoroughfare` = ?, `field_member_address_postal_code` = ?, `field_member_address_locality` = ?, `field_member_address_country` = ?, `field_member_address_administrative_area` = ? WHERE `entity_id` = ?;' , [postBody['field_member_address:thoroughfare'], postBody['field_member_address:postal_code'], postBody['field_member_address:locality'], shortCountry.two, postBody['field_member_address:administrative_area'], uid], function(err, updateAddressResponse) {
 			if (err) reject(err);
 				console.log('updateAddressResponse', updateAddressResponse)
-				resolve(updateAddressResponse);
+
+				dbConnection.query('UPDATE `field_revision_field_member_address` SET `field_member_address_thoroughfare` = ?, `field_member_address_postal_code` = ?, `field_member_address_locality` = ?, `field_member_address_country` = ?, `field_member_address_administrative_area` = ? WHERE `entity_id` = ?;' , [postBody['field_member_address:thoroughfare'], postBody['field_member_address:postal_code'], postBody['field_member_address:locality'], shortCountry.two, postBody['field_member_address:administrative_area'], uid], function(err, updateREVISIONresponse) {
+					if (err) reject(err);
+						console.log('updateAddressREVISIONResponse', updateREVISIONresponse)
+						resolve(updateREVISIONresponse);
+				 })
 		 })
 
 	});
